c495343949a1fa6c7bb30483f1e5b334ea3ba7eb
dreamscached <dreamscache.d@gmail.com>
2021-12-23 09:22:03 +0100

    Fix

--- ping/ping_test.go
+++ ping/ping_test.go
@@ -18,8 +18,8 @@ func TestPing(t *testing.T) {
 		t.Errorf("Expected protocol version 4, got %d", res.Version.Protocol)
 	}
 
-	if res.Description.(map[string]interface{})["text"].(string) != "A Minecraft Server" {
-		t.Errorf("Expected description A Minecraft Server, got %s", res.Description.(map[string]interface{})["text"].(string))
+	if res.Description.(string) != "A Minecraft Server" {
+		t.Errorf("Expected description A Minecraft Server, got %s", res.Description.(string))
 	}
 
 	if res.Players.Max != 20 {
