6d5827bf547f860609fbabc8e8b4e1655e59f1d0
dreamscached <dreamscache.d@gmail.com>
2022-05-22 13:07:31 +0200

    Fix #23

--- ping/net.go
+++ ping/net.go
@@ -7,15 +7,16 @@ import (
 )
 
 func newTCPConn(host string, port uint16, deadline time.Time) (net.Conn, error) {
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	dialer := &net.Dialer{Deadline: deadline}
+
+	conn, err := dialer.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
 		return nil, err
 	}
+
 	if err = conn.SetDeadline(deadline); err != nil {
 		return nil, err
 	}
-	if err != nil {
-		return nil, err
-	}
+
 	return conn, nil
 }
