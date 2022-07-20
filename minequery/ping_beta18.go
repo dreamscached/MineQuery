40b3366476d300933ca8620a5f89093be9fe326b
dreamscached <dreamscache.d@gmail.com>
2022-07-20 22:30:47 +0200

    Refactor beta 1.8 ping

--- minequery/ping_beta18.go
+++ minequery/ping_beta18.go
@@ -1,11 +1,11 @@
 package minequery
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"strconv"
 	"strings"
-
-	"golang.org/x/text/encoding/unicode"
 )
 
 var pingBeta18PingPacket = []byte{0xfe}
@@ -38,64 +38,91 @@ func (p Pinger) PingBeta18(host string, port int) (StatusBeta18, error) {
 		return StatusBeta18{}, err
 	}
 
-	// Write single-byte FE ping packet
-	if err = writeBytes(conn, pingBeta18PingPacket); err != nil {
-		return StatusBeta18{}, err
+	// Send ping packet
+	if err = writePingPacketBeta18(conn); err != nil {
+		return StatusBeta18{}, fmt.Errorf("could not write ping packet: %w", err)
 	}
 
-	// Read packet type, return error if it isn't FF kick packet
-	packetType, err := readByte(conn)
+	// Read status response (note: uses the same packet reading approach as 1.4)
+	content, err := readResponsePacketBeta18(conn)
 	if err != nil {
-		return StatusBeta18{}, err
-	} else if packetType != pingBeta18ResponsePacketID {
-		return StatusBeta18{}, fmt.Errorf("expected packet ID %#x, but instead got %#x", pingBeta18ResponsePacketID, packetType)
+		return StatusBeta18{}, fmt.Errorf("could not read response packet: %w", err)
+	}
+
+	// Parse response data from status packet
+	res, err := parseResponseDataBeta18(content)
+	if err != nil {
+		return StatusBeta18{}, fmt.Errorf("could not parse status from response packet: %w", err)
+	}
+
+	return res, nil
+}
+
+// Communication
+
+func writePingPacketBeta18(writer io.Writer) error {
+	// Write single-byte FE ping packet
+	err := writeBytes(writer, pingBeta18PingPacket)
+	return err
+}
+
+func readResponsePacketBeta18(reader io.Reader) (io.Reader, error) {
+	// Read packet type, return error if it isn't FF kick packet
+	id, err := readByte(reader)
+	if err != nil {
+		return nil, err
+	} else if id != pingBeta18ResponsePacketID {
+		return nil, fmt.Errorf("expected packet ID %#x, but instead got %#x", ping16ResponsePacketID, id)
 	}
 
 	// Read packet length, return error if it isn't readable as unsigned short
 	// Worth noting that this needs to be multiplied by two further on (for encoding reasons, most probably)
-	length, err := readUShort(conn)
+	length, err := readUShort(reader)
 	if err != nil {
-		return StatusBeta18{}, err
+		return nil, err
 	}
 
 	// Read remainder of the status packet as raw bytes
 	// This is a UTF-16BE string separated by § (paragraph sign)
-	// where [0] is MOTD, [1] is online players and [2] is max players
-	dataEncoded, err := readNBytes(conn, int(length*2))
-	if err != nil {
-		return StatusBeta18{}, err
+	var data bytes.Buffer
+	if _, err = io.CopyN(&data, reader, int64(length*2)); err != nil {
+		return nil, err
 	}
 
-	// Decode UTF16-BE and return error if unable to decode
-	utf16be := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewDecoder()
-	dataString, err := utf16be.Bytes(dataEncoded)
+	// Return UTF16-BE decoder with data as input
+	return utf16BEDecoder.Reader(&data), nil
+}
+
+// Response processing
+
+func parseResponseDataBeta18(reader io.Reader) (StatusBeta18, error) {
+	data, err := io.ReadAll(reader)
 	if err != nil {
 		return StatusBeta18{}, err
 	}
 
 	// Split status string, parse and map to struct returning errors if conversions fail
-	fields := strings.Split(string(dataString), pingBeta18ResponseFieldSeparator)
+	fields := strings.Split(string(data), pingBeta18ResponseFieldSeparator)
 	if len(fields) != 3 {
 		return StatusBeta18{}, fmt.Errorf("%w: expected 3 status fields, got %d", ErrInvalidStatus, len(fields))
 	}
 	motd, onlineString, maxString := fields[0], fields[1], fields[2]
 
-	// Check MOTD length
-	if p.UseStrict && len([]byte(motd)) > 64 {
-		return StatusBeta18{}, fmt.Errorf("%w: MOTD is longer than 64 bytes", ErrInvalidStatus)
-	}
-
 	// Parse online players
 	online, err := strconv.ParseInt(onlineString, 10, 32)
 	if err != nil {
-		return StatusBeta18{}, fmt.Errorf("%w: %s", ErrInvalidStatus, err)
+		return StatusBeta18{}, fmt.Errorf("%w: could not parse online players count: %s", ErrInvalidStatus, err)
 	}
 
 	// Parse max players
 	max, err := strconv.ParseInt(maxString, 10, 32)
 	if err != nil {
-		return StatusBeta18{}, fmt.Errorf("%w: %s", ErrInvalidStatus, err)
+		return StatusBeta18{}, fmt.Errorf("%w: could not parse max players count: %s", ErrInvalidStatus, err)
 	}
 
-	return StatusBeta18{MOTD: motd, OnlinePlayers: int(online), MaxPlayers: int(max)}, nil
+	return StatusBeta18{
+		MOTD:          motd,
+		OnlinePlayers: int(online),
+		MaxPlayers:    int(max),
+	}, nil
 }
