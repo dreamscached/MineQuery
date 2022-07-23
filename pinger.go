5bbb0322de8ba02c3103d21480b67948bf88bfa2
dreamscached <dreamscache.d@gmail.com>
2022-07-23 11:42:25 +0200

    Revert "Create new pinger from default one"
    This reverts commit 0bf1ce566103afcdce029cea27798f6cdb243ec5.

--- pinger.go
+++ pinger.go
@@ -68,7 +68,7 @@ type Pinger struct {
 
 // NewPinger constructs new Pinger instance optionally with additional options.
 func NewPinger(options ...PingerOption) Pinger {
-	pinger := defaultPinger
+	var pinger Pinger
 	for _, configure := range options {
 		configure(&pinger)
 	}
